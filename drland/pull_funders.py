dd6de78d95f5bb529dd661b77c24b0bca8452fc4
Fionntán O'Donnell <fionntan@fastmail.com>
2022-01-10 17:54:56 +0000

    cleaned up code

--- drland/pull_funders.py
+++ drland/pull_funders.py
@@ -4,25 +4,7 @@ from dotenv import load_dotenv
 
 from lens import Lens
 
-
-# query = "data ethics"
-
-
 query = {
-    # "query": {
-    #     "bool":{
-    #         "should": [
-    #             {"match_phrase": {"title": "data ethics"}},
-    #             {"match_phrase": {"abstract": "data ethics"}},
-    #             {"match_phrase": {"keyword": "data ethics"}},
-    #             {"match_phrase": {"field_of_study": "data ethics"}}
-    #         ],
-    #     },
-    #     "must": {"match": {"has_funding": "true"}}
-    # },
-
-    # {
-
     "query": {
         "bool": {
             "must": [
@@ -48,22 +30,7 @@ query = {
     "include": ["title", "funding"],
     # "stemming": False
 }
-    #  },
-    # "size": 1,
-    # "sort": [
-    #        {
-    #             "year_published": "desc"
-    #        }
-    # ],
-    # "include": ["title", "doi"]
-# }
-# query = {
-#     "query": {
-#         "terms": {
-#             "pmid": ["14297189", "17475107"]
-#         }
-#     }
-# }
+
 
 load_dotenv()
 token = os.environ.get("LENS_TOKEN")
@@ -71,16 +38,6 @@ lens = Lens(token)
 response = lens.query(query)
 
 print("Status code: ", response.status_code)
-# print("Printing Entire Post Request")
-# print(response.json())
-
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 pp.pprint(response.json())
-
-# from pathlib import Path
-# import filepaths
-
-# results_filepath = Path(filepaths.ROOT_DIR) / 'data' / 'results.json'
-# with open(results_filepath, 'w') as outfile:
-#     json.dump(response.json(), outfile)
