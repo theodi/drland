871b20089204b0298efc4e0fdbc851f99b7223be
Fionntán O'Donnell <fionntan@fastmail.com>
2022-01-11 12:30:50 +0000

    moved results to results folder

--- drland/filepaths.py
+++ drland/filepaths.py
@@ -2,9 +2,9 @@ from pathlib import Path
 import os
 
 ROOT_DIR = Path(os.path.realpath(os.path.join(os.path.dirname(__file__), '..')))
-WORKS_RESULTS = ROOT_DIR / 'data' / 'works_results.csv'
-WORKS_RESULTS_INCLUDE = ROOT_DIR / 'data' / 'works_results_include.csv'
-WORKS_RESULTS_EXCLUDE = ROOT_DIR / 'data' / 'works_results_exclude.csv'
+WORKS_RESULTS = ROOT_DIR / 'results' / 'works_results.csv'
+WORKS_RESULTS_INCLUDE = ROOT_DIR / 'results' / 'works_results_include.csv'
+WORKS_RESULTS_EXCLUDE = ROOT_DIR / 'results' / 'works_results_exclude.csv'
 # DATA_RESEARCH_FUNDERS_JSON = ROOT_DIR / 'data' / 'data_research_funders.json'
 
 FUNDERS_INCLUDE = ROOT_DIR / 'data' / 'funders_include.txt'
