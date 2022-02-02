e4994d4779d3c288a9ee60ce9a410c87c043998e
Fionntán O'Donnell <fionntan@fastmail.com>
2022-02-02 16:55:00 +0000

    updated results

--- drland/filepaths.py
+++ drland/filepaths.py
@@ -13,5 +13,11 @@ TOP_FUNDERS_RESULTS_EXCLUDE = ROOT_DIR / 'results' / 'top_funders_results_exclud
 FUNDERS_INCLUDE = ROOT_DIR / 'data' / 'funders_include.txt'
 FUNDERS_EXCLUDE = ROOT_DIR / 'data' / 'funders_exclude.txt'
 
+LENS_RESULTS = ROOT_DIR / 'results' / 'Lens Dataset - Pre-Cull (Ten keywords; Title, Abstract, Keyword, Field; 2012-2022; Has Funding) - Lens Dataset - Pre-Cull (Ten keywords; Title, Abstract, Keyword, Field; 2012-2022; Has Funding.csv'
+LENS_RESULTS_FILTER = ROOT_DIR / 'results' / 'Lens Dataset (Ten keywords; Title, Abstract, Keyword, Field; 2012-2022; Has Funding; Limited to only UK funders).csv'
+# LENS_RESULTS_FILTER_NOT = ROOT_DIR / 'results' / 'Lens Dataset - NOT INCLUDED Post-Cull (Ten keywords; Title, Abstract, Keyword, Field; 2012-2022; Has Funding) - Lens Dataset - Pre-Cull (Ten keywords; Title, Abstract, Keyword, Field; 2012-2022; Has Funding.csv'
+
+ALL_FUNDERS_LENS_RESULTS = ROOT_DIR / 'results' / 'all_funders_in_lens_results.csv'
+
 GTR_PROJECTS = ROOT_DIR / 'data' / 'projectsearch-1643043544989.csv'
 
