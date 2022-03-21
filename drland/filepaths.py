09d8d41e679f8c7106d66995ff0e6d1299f7a34f
Fionntán O'Donnell <fionntan@fastmail.com>
2022-03-21 10:05:38 +0000

    removed all the data files no longer needed

--- drland/filepaths.py
+++ drland/filepaths.py
@@ -2,8 +2,4 @@ from pathlib import Path
 import os
 
 ROOT_DIR = Path(os.path.realpath(os.path.join(os.path.dirname(__file__), '..')))
-FUNDERS_INCLUDE = ROOT_DIR / 'data' / 'funders_include.txt'
-FUNDERS_EXCLUDE = ROOT_DIR / 'data' / 'funders_exclude.txt'
-
-GTR_PROJECTS = ROOT_DIR / 'data' / 'projectsearch-1643043544989.csv'
 
